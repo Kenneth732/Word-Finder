2cadd76fb1e8bc0f1ed321c789211273218362a2
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-02 16:28:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// Get the required elements from the DOM
+const commonWordForm = document.getElementById('common-word-form');
+const textInput = document.getElementById('text-input');
+const resultElement = document.getElementById('result');
+
+// Function to find the most common word
+function findMostCommonWord(event) {
+    event.preventDefault();
+
+    const text = textInput.value;
+    const words = text.trim().split(/\s+/);
+    const wordCounts = {};
+
+    words.forEach(function (word) {
+        if (wordCounts[word]) {
+            wordCounts[word]++;
+        } else {
+            wordCounts[word] = 1;
+        }
+    });
+
+    let mostCommonWord = '';
+    let maxCount = 0;
+
+    for (const word in wordCounts) {
+        if (wordCounts[word] > maxCount) {
+            mostCommonWord = word;
+            maxCount = wordCounts[word];
+        }
+    }
+
+    resultElement.textContent = `Most Common Word: ${mostCommonWord} (${maxCount} occurrences)`;
+}
+
+// Add event listener to the form submission
+commonWordForm.addEventListener('submit', findMostCommonWord);
